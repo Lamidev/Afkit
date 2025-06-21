1c10c2b848f0b08d0e40696f32fa8391d51042fd
thisslami <akinyemioluwaseunjunior@gmail.com>
2025-06-21 20:21:41 +0100

    changes again

--- Server/routes/common/sitemap-routes.js
+++ Server/routes/common/sitemap-routes.js
@@ -13,7 +13,6 @@ router.get("/sitemap.xml", async (req, res) => {
       { loc: `${BASE_URL}/shop/home`, lastmod: now, priority: 1.0 },
       { loc: `${BASE_URL}/shop/listing`, lastmod: now, priority: 0.9 },
       { loc: `${BASE_URL}/shop/about`, lastmod: now, priority: 0.7 },
-      { loc: `${BASE_URL}/shop/search`, lastmod: now, priority: 0.5 }
     ];
 
     const productUrls = products.map(product => ({
@@ -40,39 +39,5 @@ ${[...staticPages, ...productUrls].map(url => `
     res.status(500).send("Error generating sitemap");
   }
 });
-
-// router.get("/sitemap.xml", async (req, res) => {
-//   try {
-//     const products = await Product.find({}, "_id"); // or use slug if available
-
-//     let urls = [
-//       `${BASE_URL}/shop/home`,
-//       `${BASE_URL}/shop/listing`,
-//       `${BASE_URL}/shop/about`,
-//       `${BASE_URL}/shop/search`,
-//     ];
-
-//     products.forEach((product) => {
-//       urls.push(`${BASE_URL}/shop/product/${product._id}`);
-//     });
-
-//     const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
-// <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-// ${urls
-//   .map(
-//     (url) => `<url>
-//   <loc>${url}</loc>
-// </url>`
-//   )
-//   .join("\n")}
-// </urlset>`;
-
-//     res.header("Content-Type", "application/xml");
-//     res.send(sitemapXml);
-//   } catch (err) {
-//     console.error("Failed to generate sitemap", err);
-//     res.status(500).send("Error generating sitemap");
-//   }
-// });
 
 module.exports = router;
